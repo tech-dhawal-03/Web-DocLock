622253be96f834d825735ab57a2e1d6643753a8f
Deepanshu Pathak <dhruvpathak365@gmail.com>
2024-07-16 22:09:40 +0530

    Password page added

--- My React App/src/App.jsx
+++ My React App/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero_Page";
 import "./index.css";
 import Login from "./components/Login";
@@ -7,19 +6,24 @@ import { Routes, Route } from "react-router-dom";
 import Error from "./components/Error";
 import Signup from "./components/Signup";
 import CardHome from "./components/CardHome";
+import CardHelp from "./components/CardHelp";
+import CardDocument from "./components/CardDocument";
+import CardPassword from "./components/CardPassword";
 
 export default function App() {
   return (
     <>
-      
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Error />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login-successful" element={<CardHome/>} />
+        <Route path="/cardprofile" element={<CardHome/>} />
         <Route path="/logout-successful" element={<Hero/>} />
-        
+        <Route path="/cardhelp" element={<CardHelp />} />
+        <Route path="/cardprofile" element={<CardHome />} />
+        <Route path="/carddocument" element={<CardDocument/>} />
+        <Route path="/cardpassword" element={<CardPassword />} />        
       </Routes>
     </>
   );
